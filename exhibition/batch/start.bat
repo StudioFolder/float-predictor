59ee94164f3c8f4d5b50a71e48b8a3a3fb2c44cc
Iacopo Leardini <iacopo.lea@gmail.com>
2018-03-18 18:03:33 +0100

    updated start script

--- exhibition/batch/start.bat
+++ exhibition/batch/start.bat
@@ -1,10 +1,13 @@
 taskkill /im node.exe
 taskkill /im chrome.exe
-cd C:\Users\username\Documents\aerocene\
+cd C:\Users\Saraceno\aerocene\
 git pull
 npm install
+cd exhibition\
+npm install
+cd ..
 npm run build
-cd exhibition
-start node server.js
+cd exhibition\
+start node index.js
 timeout /t 10
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -kiosk -fullscreen http://localhost:1337/#/server
