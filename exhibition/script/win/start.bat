3ade9e6b577c5404081735cfb8206d337dbaf9fd
Angelo Semeraro <angeloseme@gmail.com>
2018-06-15 22:59:37 +0200

    adding windows scripts

--- exhibition/script/win/start.bat
+++ exhibition/script/win/start.bat
@@ -1,13 +1,16 @@
+cd ..\..\..\
+cd
 taskkill /im node.exe
 taskkill /im chrome.exe
-cd C:\Users\Saraceno\aerocene\
+timeout /t 5
 git pull
-npm install
+cmd.exe /c npm install
+timeout /t 5
+cmd.exe /c npm run build
+timeout /t 5
 cd exhibition\
-npm install
-cd ..
-npm run build
-cd exhibition\
+cmd.exe /c npm install
+timeout /t 5
 start node index.js
-timeout /t 10
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -kiosk -fullscreen http://localhost:1337/#/server
+timeout /t 20
+start chrome.exe --kiosk --fullscreen http://localhost:1337/#/server
